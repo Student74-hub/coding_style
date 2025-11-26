280b26b353acb76277a0b0a140b21d36a79cb180
Valeriy <student174471@gmail.com>
2025-11-26 20:21:15 +0300

    style: добавление кратких комментариев и группировка объявлений

--- mycomplex.h
+++ mycomplex.h
@@ -3,40 +3,56 @@
 #include <iostream>
 using namespace std;
 
+// Класс Complex представляет комплексное число и основные операции над ним.
 class Complex
 {
+private:
     double Re;
     double Im;
 
 public:
+    // Конструкторы и деструктор
     Complex( double aRe = 0, double aIm = 0 );
     Complex( const Complex& );
     ~Complex();
+
+    // Методы класса
     void Set( double aRe, double aIm = 0 );
     operator double();
     double abs();
 
+    // Операторы ввода-вывода
     friend istream& operator>>( istream&, Complex& );
     friend ostream& operator<<( ostream&, Complex& );
 
+    // Арифметические операторы с комплексными числами
     Complex operator+( const Complex& );
     Complex operator-( const Complex& );
-    Complex operator+( const double& );
-    friend Complex operator+( const double&, const Complex& );
-    Complex operator-( const double& );
-    friend Complex operator-( const double&, const Complex& );
     Complex operator*( const Complex& );
+
+    // Арифметические операторы с вещественными числами
+    Complex operator+( const double& );
+    Complex operator-( const double& );
     Complex operator*( const double& );
-    friend Complex operator*( const double&, const Complex& );
     Complex operator/( const double& );
 
+    // Дружественные арифметические операторы
+    friend Complex operator+( const double&, const Complex& );
+    friend Complex operator-( const double&, const Complex& );
+    friend Complex operator*( const double&, const Complex& );
+
+    // Операторы присваивания с комплексными числами
     Complex& operator+=( const Complex& );
     Complex& operator-=( const Complex& );
     Complex& operator*=( const Complex& );
+
+    // Операторы присваивания с вещественными числами
     Complex& operator+=( const double& );
     Complex& operator-=( const double& );
     Complex& operator*=( const double& );
     Complex& operator/=( const double& );
+
+    // Операторы присваивания
     Complex& operator=( const Complex& );
     Complex& operator=( const double& );
 };
